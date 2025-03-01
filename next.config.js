229bd2b5517fabb37f4eb91f01190cda7c7d6ab9
thiagosouzasite <thiago@thiagosouza.site>
2025-03-01 17:10:42 -0300

    Corrige configurações para deploy

--- next.config.js
+++ next.config.js
@@ -1,12 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  // Especifique o hostname explicitamente:
-  serverOptions: {
-    hostname: '0.0.0.0',
-    port: 3000
-  }
+  swcMinify: true
 }
 
 module.exports = nextConfig 
